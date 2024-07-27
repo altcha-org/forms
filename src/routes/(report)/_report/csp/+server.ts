1dd094782dc9a21fd75de7ceec38b44917ea225e
Daniel Regeci <536331+ovx@users.noreply.github.com>
2024-07-27 07:53:12 -0300

    csp on by default + report uri

---
+++ src/routes/(report)/_report/csp/+server.ts
@@ -0,0 +1,17 @@
+import { requestHandler } from '$lib/server/handlers';
+import { rateLimit } from '$lib/server/ratelimiter';
+import { logger } from '$lib/server/logger';
+import type { RequestHandler } from './$types';
+
+export const POST = requestHandler(
+	async (event) => {
+		await rateLimit('L3', event);
+		const body = await event.request.json();
+		if (body['csp-report']) {
+			logger.warn('CSP REPORT %o', body);
+		}
+	},
+	{
+		authorization: false,
+	}
+) satisfies RequestHandler;
